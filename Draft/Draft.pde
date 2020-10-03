f94f75b0b08c5510dd6886f68ab6e11c7457b44b
U-dot <71671790+U-dot@users.noreply.github.com>
2020-10-03 07:43:14 -0500

    Nuevas funciones extra. Bug.
    1. randomFill(matrix[][])
    2. print Matrix[][]
    3. removeRow(Matrix[][])
    Hay un bug: no imprime las filas borradas (de0s) porque la función removeRow no devuelve la matriz que es.

--- Draft/Draft.pde
+++ Draft/Draft.pde
@@ -1,6 +1,8 @@
+
+
 //Número de filas y columnas
-final int ROWS=20;
-final int COLS=10;
+final int ROWS=3;
+final int COLS=4;
 
 //Crea una matriz de ROWSxCOLS,valores se incializan en 0
 int [][] tableau = new int [ROWS][COLS];
@@ -25,12 +27,16 @@ int t=0;//Lleva cuenta del tiempo
 
 void setup() {
   size(600, 750);
-  print(Tetrominoes);
+  randomFill(tableau);
+  print(tableau);
+  removeRow(0,tableau);
+  print(tableau);
+
 }
 
 void draw() {
   background(125);
-  drawTetrominoe(Tetrominoes[tetro]);
+  //drawTetrominoe(Tetrominoes[tetro]);
 }
 
 void drawTetrominoe(int[] A) {
@@ -43,9 +49,9 @@ void drawTetrominoe(int[] A) {
     if ( ( A[A[pos-1]] & (1 << 15 - i) ) != 0) {
       //& Compara bit por bit: si son iguales->1 si son diferentes ->0
       rect( (i % 4)*width/ROWS+A[1]*width/ROWS,
-            (i/4+A[2])* width/ROWS,
+            (i/4+A[2])* height/COLS,
             width/ROWS,
-            width/ROWS);
+            height/COLS);
     }
   }
   pop();
@@ -84,7 +90,39 @@ void keyPressed() {
 //Función imprimir matriz
 void print(int matrix [][]){
   for (int i=0; i<matrix.length; i++){
-    print("row i");
+    print("row",i,"\n");
     println(matrix[i]);
+  }
+}
+
+//Para matrices rectangulares
+//Remover fila y devolver matriz modificada
+int[][]removeRow(int delRow,int matrix[][]){
+  int rows=matrix.length;
+  int cols=matrix[0].length;
+  print("rows",rows);
+  if (delRow<0||delRow>=rows){
+    throw new Error("removeRow out of bounds");
+  }
+  int counter=0;
+  int [][] matrix1 = new int [rows][cols];
+  //for(int i=0;i<rows;i++){
+  //  //if (delRow==i){i++;}
+  //  for(int j=0;j<cols;j++){
+  //    matrix1[i][j]=matrix[i][j];
+  //  }
+  //  counter++;
+  //}
+  print (matrix1);
+  return matrix1;
+}
+
+void randomFill(int matrix[][]){
+  int rows=matrix.length;
+  int cols=matrix[0].length;
+  for(int i=0;i<rows;i++){
+    for(int j=0;j<cols;j++){
+      matrix[i][j]=int(random(100));
+    }
   }
 }
--- Draft/build/source/Draft.java
+++ Draft/build/source/Draft.java
@@ -14,24 +14,26 @@ import java.io.IOException;
 
 public class Draft extends PApplet {
 
-//Número de filas y columnas
-final int ROWS=20;
-final int COLS=10;
 
-//Crea una matriz de ROWSxCOLS
+
+//Número de filas y columnas
+final int ROWS=3;
+final int COLS=4;
+
+//Crea una matriz de ROWSxCOLS,valores se incializan en 0
 int [][] tableau = new int [ROWS][COLS];
 //Guarda la posición de la primera rotación en todos los tetrominos
 int pos=4;
 
 //Información de cada tetromino
 //Color, posX, posY, Posición de rotación+2, Rotación 0, Rotación 1...
-int [] T = {0xffFF007D, 0,-2, 2, 114, 610, 624, 562};
-int [] I = {0xff38FAEE, 0,-2, 2, 240, 8738};
-int [] J = {0xff383CFA, 0,-2, 2, 550, 1136, 802, 113};
-int [] L = {0xffFFA939, 0,-2, 2, 547, 116, 1570, 368};
-int [] S = {0xff70FF39, 0,-2, 2, 54, 561};
-int [] Z = {0xffFF1F1F, 0,-2, 2, 99, 612};
-int [] O = {0xffE60DFF, 0,-2, 2, 51};
+int [] T = {0xffFF007D, 0,-2, 4, 114, 610, 624, 562};
+int [] I = {0xff38FAEE, 0,-2, 4, 240, 8738};
+int [] J = {0xff383CFA, 0,-2, 4, 550, 1136, 802, 113};
+int [] L = {0xffFFA939, 0,-2, 4, 547, 116, 1570, 368};
+int [] S = {0xff70FF39, 0,-2, 4, 54, 561};
+int [] Z = {0xffFF1F1F, 0,-2, 4, 99, 612};
+int [] O = {0xffE60DFF, 0,-2, 4, 51};
 
 //Lista con todos los tetrominos
 int[] [] Tetrominoes= {T, I, J, L, S, Z, O};
@@ -41,35 +43,40 @@ int t=0;//Lleva cuenta del tiempo
 
 public void setup() {
   
+  randomFill(tableau);
+  print(tableau);
+  removeRow(0,tableau);
+  print(tableau);
+
 }
 
 public void draw() {
   background(125);
-  drawTetrominoe();
+  //drawTetrominoe(Tetrominoes[tetro]);
 }
 
-public void drawTetrominoe() {
+public void drawTetrominoe(int[] A) {
   push();
   strokeWeight(5);
-  fill(Tetrominoes[tetro][0]);
+  fill(A[0]);
   //Se encarga de pintar el cuadradito si se debe
   for (int i = 0; i <= 15; i++) {
     //value<<n value to shift n: number of places to shift
-    if ( ( Tetrominoes[tetro][Tetrominoes[tetro][pos-1]] & (1 << 15 - i) ) != 0) {
+    if ( ( A[A[pos-1]] & (1 << 15 - i) ) != 0) {
       //& Compara bit por bit: si son iguales->1 si son diferentes ->0
-      rect( (i % 4)*width/ROWS+Tetrominoes[tetro][1]*width/ROWS,
-            (i/4+Tetrominoes[tetro][2])* width/ROWS,
+      rect( (i % 4)*width/ROWS+A[1]*width/ROWS,
+            (i/4+A[2])* height/COLS,
             width/ROWS,
-            width/ROWS);
+            height/COLS);
     }
   }
   pop();
   if(millis()>600*t){
     t++;
-    Tetrominoes[tetro][2]++;
-  }if(Tetrominoes[tetro][2]>ROWS){
-    Tetrominoes[tetro][2]=-2;
-    Tetrominoes[tetro][1]=PApplet.parseInt(random(ROWS-3));
+    A[2]++;
+  }if(A[2]>ROWS){
+    A[2]=-2;
+    A[1]=PApplet.parseInt(random(ROWS-3));
     tetro=PApplet.parseInt(random(7));
     background(0);
   }
@@ -88,9 +95,50 @@ public void keyPressed() {
     }
     if (Tetrominoes[tetro][pos-1] < pos) {
       Tetrominoes[tetro][pos-1]=Tetrominoes[tetro].length-1 ;
+    }if(Tetrominoes[tetro][1]<-3){
+      Tetrominoes[tetro][1]=ROWS-1;
+    }else if(ROWS-1<Tetrominoes[tetro][1]){
+      Tetrominoes[tetro][1]=-3;
     }
-    else {
-      Tetrominoes[tetro][pos-1]=(Tetrominoes[tetro][pos-1]-pos) % (Tetrominoes[tetro].length-pos) +pos;
+
+  }
+}
+//Función imprimir matriz
+public void print(int matrix [][]){
+  for (int i=0; i<matrix.length; i++){
+    print("row",i,"\n");
+    println(matrix[i]);
+  }
+}
+
+//Para matrices rectangulares
+//Remover fila y devolver matriz modificada
+public int[][]removeRow(int delRow,int matrix[][]){
+  int rows=matrix.length;
+  int cols=matrix[0].length;
+  print("rows",rows);
+  if (delRow<0||delRow>=rows){
+    throw new Error("removeRow out of bounds");
+  }
+  int counter=0;
+  int [][] matrix1 = new int [rows][cols];
+  //for(int i=0;i<rows;i++){
+  //  //if (delRow==i){i++;}
+  //  for(int j=0;j<cols;j++){
+  //    matrix1[i][j]=matrix[i][j];
+  //  }
+  //  counter++;
+  //}
+  print (matrix1);
+  return matrix1;
+}
+
+public void randomFill(int matrix[][]){
+  int rows=matrix.length;
+  int cols=matrix[0].length;
+  for(int i=0;i<rows;i++){
+    for(int j=0;j<cols;j++){
+      matrix[i][j]=PApplet.parseInt(random(100));
     }
   }
 }
